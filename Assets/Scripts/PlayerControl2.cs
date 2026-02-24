3af2b87bd97b5dfdb341eb78cb5ec77f3548adf0
forest-glade <25426605@studentmail.ul.ie>
2026-02-24 22:22:35 +0000

    new stuff
    added basic attack and axe prop

--- Assets/Scripts/PlayerControl2.cs
+++ Assets/Scripts/PlayerControl2.cs
@@ -12,9 +12,16 @@ public class PlayerControl2 : MonoBehaviour
     //hold the length of the animation
     [SerializeField] float moveSpeed = 4;
     [SerializeField] float sprintSpeed = 10;
+    [SerializeField] float dodgeSpeed = 30;
     [SerializeField] float rotateSpeed = 15;
     [SerializeField] int gravity = 25;
 
+    //for ground detection
+    [Header("Ground Detection")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundRadius;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private bool isGrounded;
 
     float velocityY;
     CharacterController CharacterControl;
@@ -35,6 +42,10 @@ public class PlayerControl2 : MonoBehaviour
     }
      void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position,groundRadius, (int)whatIsGround);
+        //Debug.Log(isGrounded);
+        //Debug.Log(isDodging);
+
         RecordControls();
         // disable character movement while rolling
         if(!isDodging)PlayerMovement();
@@ -42,14 +53,16 @@ public class PlayerControl2 : MonoBehaviour
         // dodge if button is pressed
         if (Input.GetKeyDown(KeyCode.F)) 
         {
-            if (direction.magnitude !=0) StartCoroutine(Dodge());
+            if (direction.magnitude !=0 && isDodging==false) StartCoroutine(Dodge());
             { }
         
         }
     }
     IEnumerator Dodge() {
+        isDodging = true;
         anim.SetTrigger("Dodge");
-        isDodging =true;
+        Debug.Log(isDodging);
+      
         float timer = 0;
         
         while (timer < dodgeTimer) {
@@ -57,9 +70,10 @@ public class PlayerControl2 : MonoBehaviour
             Vector3 dir = (transform.forward * speed + (Vector3.up * velocityY));
             CharacterControl.Move(dir*Time.deltaTime);
                 timer += Time.deltaTime;
+            
             yield return null;
         }
-    isDodging = false;
+        isDodging = false;
     }
     void RecordControls() { moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     Vector3 forward = cam.forward;
@@ -70,7 +84,16 @@ public class PlayerControl2 : MonoBehaviour
         right.Normalize();
 
         direction = (right*moveInput.x + forward*moveInput.y).normalized;
-        anim.SetFloat("moveAmount", direction.magnitude, 0.1f, Time.deltaTime);
+        float moveAmount = 0f;
+        if (direction.magnitude > 0.1f)
+        { //sprint animation
+            if (Input.GetKey(KeyCode.LeftShift)) { moveAmount = 1f; }
+            //walk animation
+            else { moveAmount = 0.5f; }
+        }
+        //idle
+        else { moveAmount = 0f; }
+            anim.SetFloat("moveAmount", moveAmount, 0.1f, Time.deltaTime);
     }
     void PlayerMovement() { 
         
@@ -79,7 +102,9 @@ public class PlayerControl2 : MonoBehaviour
         Vector3 fallVelocity = Vector3.up * velocityY;
        
         if (Input.GetKey(KeyCode.LeftShift)) { Vector3 velocity = (direction * sprintSpeed) + fallVelocity; CharacterControl.Move(velocity * Time.deltaTime); }
-        else { Vector3 velocity = (direction * moveSpeed) + fallVelocity; CharacterControl.Move(velocity * Time.deltaTime); }
+        else if(!Input.GetKey(KeyCode.LeftShift)&&!isDodging) { Vector3 velocity = (direction * moveSpeed) + fallVelocity; CharacterControl.Move(velocity * Time.deltaTime); }
+        else if(isDodging) { Vector3 velocity = (direction * dodgeSpeed) + fallVelocity; CharacterControl.Move(velocity * Time.deltaTime); }
+        //else { Vector3 velocity = (direction * moveSpeed) + fallVelocity; CharacterControl.Move(velocity * Time.deltaTime); }
         
             
 
